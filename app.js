21cf07422da35e229ad8946c586e41eb8bbda3b6
Enez Akcan <113882242+EnezAkcan@users.noreply.github.com>
2023-02-06 16:55:36 +0300

    yesno

---
+++ app.js
@@ -0,0 +1,44 @@
+
+var answer = document.getElementById("question-ask");
+
+function createAlert(e) {
+    var x = e.keyCode;
+    switch (x) {
+      case 191:
+        getApiContent();
+        goBack();
+        hide();
+        console.log('success');
+    }
+    console.log(x);
+  }
+document.addEventListener('keydown', createAlert);
+
+const urll = 'https://yesno.wtf/api';
+let localUrl = 'http://127.0.0.1:5500/';
+
+const apiContentList = [];
+
+function getApiContent(){
+  fetch('https://yesno.wtf/api')
+  .then(response => response.json())
+  .then(data => {                 
+    if(data){
+      apiContentList.push(data);
+    }
+    console.log(data.image)
+    console.log(data.answer)
+    document.getElementById("myimg").setAttribute("src",data.image);
+    document.getElementById("answer").innerHTML = data.answer;
+  })
+}
+function goBack(){
+  setTimeout(function(){
+    document.location = localUrl }
+  ,3000)
+}
+
+function hide(){
+  document.getElementById("answer_question").style.display = "none";
+} 
+
